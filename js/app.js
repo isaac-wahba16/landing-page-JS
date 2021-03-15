5f31f2ac2610db88cae276c6f08d902eb69aa3ac
isaac-wahba16 <78953548+isaac-wahba16@users.noreply.github.com>
2021-03-15 08:15:05 +0200

    Adding project files
    All project files added

---
+++ js/app.js
@@ -0,0 +1,50 @@
+/** @format */
+
+const sections = document.querySelectorAll('section');
+const navigationUl = document.getElementById('navbar__list');
+
+// build the nav
+addNavElements = () => {
+  const sectionsNavFragment = document.createDocumentFragment();
+  sections.forEach((section) => {
+    const navElement = document.createElement('li');
+    const linkToSection = document.createElement('a');
+    linkToSection.innerText = section.getAttribute('data-nav');
+    const sectionId = section.getAttribute('id');
+    // Scroll to anchor ID using scrollTO event
+    linkToSection.setAttribute('href', `#${sectionId}`);
+    navElement.appendChild(linkToSection);
+    sectionsNavFragment.appendChild(navElement);
+  });
+  navigationUl.appendChild(sectionsNavFragment);
+};
+
+// Build menu
+addNavElements();
+
+/**
+ * End Main Functions
+ * Begin Events
+ *
+ */
+
+// Set sections as active
+const allLinks = document.querySelectorAll('a');
+manageActiveSection = () => {
+  let sectionsNumber = sections.length;
+
+  while (
+    --sectionsNumber &&
+    window.scrollY + 50 < sections[sectionsNumber].offsetTop
+  ) {}
+
+  allLinks.forEach((link) => link.classList.remove('active'));
+  // Add class 'active' to section when near top of viewport
+
+  allLinks[sectionsNumber].classList.add('active');
+};
+
+manageActiveSection();
+// Scroll to section on link click
+
+window.addEventListener('scroll', manageActiveSection);
